39915397295cae48631d2194eb5cd4a2a1119ff7
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-29 22:58:08 +0700

    no message

---
+++ LAB02_exercise/Core/Inc/ex1.h
@@ -0,0 +1,13 @@
+/*
+ * ex1.h
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+//todo2
+
+#endif /* INC_EX1_H_ */
---
+++ LAB02_exercise/Core/Src/ex1.c
@@ -0,0 +1,8 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Sep 29, 2024
+ *      Author: Admin
+ */
+
+// TODO 2
