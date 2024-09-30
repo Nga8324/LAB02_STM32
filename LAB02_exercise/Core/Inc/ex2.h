2684c42e9d9c4cc0be03abcad5ab411ca524269d
Nga8324 <nga.nguyenthithuy@hcmut.edu.vn>
2024-09-30 18:03:25 +0700

    no message

---
+++ LAB02_exercise/Core/Inc/ex2.h
@@ -0,0 +1,21 @@
+/*
+ * ex2.h
+ *
+ *  Created on: Sep 30, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX2_H_
+#define INC_EX2_H_
+
+#include "main.h"
+
+extern int timer1_flag;
+extern int timer2_flag;
+
+void display7SEG();
+void timerRun();
+void setTimer1(int duration1);
+void setTimer2(int duration2);
+
+#endif /* INC_EX2_H_ */
